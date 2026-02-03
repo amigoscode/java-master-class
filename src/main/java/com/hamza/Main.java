24bf5ef12a722759d746789157a99bbbe395da4f
Hamza Khan <khan_hamza95@hotmail.com>
2026-02-03 15:26:51 -0500

    doing inital implementation

---
+++ src/main/java/com/hamza/Main.java
@@ -0,0 +1,22 @@
+package com.hamza;
+// TODO 1. create a new branch called initial-implementation
+// TODO 2. create a package with your name. i.e com.franco and move this file inside the new package
+// TODO 3. implement https://amigoscode.com/learn/java-cli-build/lectures/3a83ecf3-e837-4ae5-85a8-f8ae3f60f7f5
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi! Welcome to your car booking app. ");
+        System.out.println("Please select one of the options to get started: ");
+        System.out.println("1\uFE0F⃣ - Book Car");
+        System.out.println("2\uFE0F⃣ - View All User Booked Cars");
+        System.out.println("3\uFE0F⃣ - View All Bookings");
+        System.out.println("4\uFE0F⃣ - View Available Cars");
+        System.out.println("5\uFE0F⃣ - View Available Electric Cars");
+        System.out.println("6\uFE0F⃣ - View all users");
+        System.out.println("7\uFE0F⃣ - Exit");
+
+    }
+
+}
